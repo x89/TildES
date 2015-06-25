300d57394e11685d16b5743b9d315717255ba16e
David Trail <napalm10@gmail.com>
2015-06-25 17:07:40 +0100

    Tidy up, resume work of 2 years ago. Deaccent function to remove tildes/accents from words

--- cmd.py
+++ cmd.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
-import sys, re, random
+
+'''A script to help Spanish learners practice word stresses.
+
+In spanish the stress is within the syllable containing the character with the
+stress, there is at most one in any word, the stressed syllable is always in
+the last 4 syllables and referred to as:
+ Aguda (stress on the final syllable)
+ Grave/Llana (stress on the penultimate syllable)
+ Esdrújula (3rd to last)
+ Sobresdrújula (4th+ syllable)
+'''
+
+import sys
+import re
+import random
 
 filename = "tilde_words"
 if len(sys.argv) > 1:
@@ -16,13 +30,15 @@ while word:
     word = read_stripped_line(fh)
 fh.close()
 
-words = random.sample(words, 20)
 
-# áéíóú > aeiou
-rx_áéíóú = re.compile(r'(.*)([áéíóú])(.*)')
-for word in words:
-    aeiou = re.search(rx_áéíóú, word)
-    print(aeiou.groups())
+def deaccent(word):
+    '''Replace our accented character with a non-accented character.'''
+    word = word.replace('á', 'a')
+    word = word.replace('é', 'e')
+    word = word.replace('í', 'i')
+    word = word.replace('ó', 'o')
+    word = word.replace('ú', 'u')
+    return word
 
 def syllabicate(word):
     """Splits words into their syllabicated form, for example the words:
@@ -36,3 +52,10 @@ def syllabicate(word):
     Good explanation: http://i1stspanish.com/syllabication-silabeo-lesson-two
     """
     return(['Th', 'is', 'Is', 'Not', 'Com', 'plete'])
+
+words = random.sample(words, 20)
+rx_áéíóú = re.compile(r'(.*)([áéíóú])(.*)')
+for word in words:
+    aeiou = re.search(rx_áéíóú, word)
+    print(aeiou.groups(), deaccent(word))
+
