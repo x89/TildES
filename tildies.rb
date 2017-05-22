062b2ec28572911c03fc161dd329d296827a1bb4
David John <github@vaunt.eu>
2017-05-22 15:31:58 +0100

    Gamify and remove useless String overriding

--- tildies.rb
+++ tildies.rb
@@ -1,13 +1,5 @@
 #!/usr/bin/env ruby
 
-class String
-  def vowel_indexes
-    '''Array of indexes of vowels'''
-    vowels = %w{a e i o u A E I O U á é í ó ú Á É Í Ó Ú}
-    self.chars.each.map { |c| vowels.include? c }
-  end
-end
-
 f = open("tilde_words")
 words = f.readlines
 f.close()
@@ -20,26 +12,49 @@ tilde_vowels = %w{á é í ó ú Á É Í Ó Ú}
 to_tilde = Hash[vowels.collect{|v| [v, tilde_vowels[vowels.index(v)]]}]
 from_tilde = Hash[tilde_vowels.collect{|v| [v, vowels[tilde_vowels.index(v)]]}]
 
-count = 5
+puts "Welcome to Nay's spanish tilde test!"
+puts "####################################"
+print "How many words would you like to attempt? "
+count = gets.chomp.to_i
+puts "Great! Let's go:"
+puts
+
+correct_answers = 0
 
 1.upto(count) do |i|
   word = words[i]
   index = nil
-  vowel_indexes = word.vowel_indexes
   helper_string = String.new
+  vowel_index = 0  # The index in vowels
+  found = false
   word.chars.each_with_index do |c,idx|
     if vowels.push(tilde_vowels).flatten.include? c
       helper_string += "↓"
+      vowel_index += 1 unless found
     else
       helper_string += " "
     end
     if tilde_vowels.include? c
+      found = true
+      vowel_index = vowel_index
       index = idx
     end
   end
   neutered_word = word.chars.map { |c| from_tilde[c] || c }.join
-  
-  puts
+
+  puts "------------------------ (#{i})"
   puts helper_string
-  puts "#{neutered_word} #{word}"
+  puts "#{neutered_word}"
+  print "Which # of verb contains the stress? "
+  answer = gets.chomp.to_i
+  if answer == vowel_index
+    puts "Correct! The word was #{word}"
+    correct_answers += 1
+  else
+    puts "Sorry, the stress was on vowel #{vowel_index} and the word was #{word}"
+  end
+  puts
 end
+
+puts
+puts "You scored #{correct_answers} out of #{count}, or #{((correct_answers.to_f/count)*100).round}%"
